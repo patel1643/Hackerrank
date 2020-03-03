5900486b57259cfc818d69226f084b6c0ea45db8
patel1643 <42159154+patel1643@users.noreply.github.com>
2020-03-03 18:08:30 -0500

    compareArrays

---
+++ insertNodeAtTail.py
@@ -0,0 +1,41 @@
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    newNode = SinglyLinkedListNode(data)
+
+    if head == None:
+        head = newNode
+    else:
+        curr = head
+        while True:
+            if curr.next == None:
+                curr.next = newNode
+                break
+            curr = curr.next
+    return head
